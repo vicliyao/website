fcd1b0500655d75e19bf15e1d68c43d1d2244796
Yao(Victor) Li <victorliyao@gmail.com>
2023-04-28 01:19:40 -0700

    Update main.js

--- silly-story/main.js
+++ silly-story/main.js
@@ -38,8 +38,8 @@ function result() {
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300*0.07142857) + 'stone';
-    const temperature =  Math.round((94-32)*(5/9)) + 'centigrade';
+    const weight = Math.round(300*0.07142857) + ' stone';
+    const temperature =  Math.round((94-32)*(5/9)) + ' centigrade';
     newStory = newStory.replaceAll('300 pounds', weight);
     newStory = newStory.replaceAll('94 fahrenheit', temperature);
 
