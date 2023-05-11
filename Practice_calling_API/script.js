ae006254c1fb8244c63e4486ebc9c9baadc1cbc7
Yao(Victor) Li <victorliyao@gmail.com>
2023-05-10 18:43:51 -0700

    update

--- Practice_calling_API/script.js
+++ Practice_calling_API/script.js
@@ -5,11 +5,11 @@ const app = express();
 
 const API_KEY = "44410596c1e0b580b79b3679c28c51f8";
 
-app.get('/', (req, res) => {
+app.get('/weather/:lat/:lon', (req, res) => {
   res.send('Hello World!');
   console.log("welcome to the root!");
-  var lat = "37.77493";
-  var lon = "-122.41942";
+  var lat = req.params.lat;
+  var lon = req.params.lon;
   
   var url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=imperial`;
   
@@ -23,18 +23,20 @@ app.get('/', (req, res) => {
 		
         
 		// Printing TEMP
-        const data = JSON.parse(body);
-		console.log(data.main.temp);
+        const body = JSON.parse(body);
+        
+        res.send({"temperature": body.main.temp, "WeatherStatus":body.weather[0].main});
+		console.log(body.main.temp);
 	});
 });
 
 
 // Extra credit: 5 Day
-app.get('/5day', (req, res) => {
+app.get('/5day/:lat/:lon', (req, res) => {
     res.send('Hello World!');
     console.log("5-day forecast(In Fahrenheit)");
-    var lat = "37.77493";
-    var lon = "-122.41942";
+    var lat = "req.params.lat";
+    var lon = "req.params.lon";
     
     var url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=imperial`;
     
@@ -48,39 +50,27 @@ app.get('/5day', (req, res) => {
           
           // Calculate Average TEMP for each day
           const data = JSON.parse(body);
-          var temp = []; //Store all temp from response
-          var averages = []; //Store Average temp after calculation
-          
-          //Get all temps from response
-          for (let i=0;i<data.list.length;i++){
-            temp.push(data.list[i].main.temp) 
-          }
-          
-          // --------------
-          // For first day
-          var A1 =0; //Initialize Average value for day1
-          for (let i=0; i<6; i++){
-            A1 += temp[i]
-          }
-          averages.push(A1/6); //Store in list
-          
-          //---------------
-          // For Day 2 - 5
-          let sum = 0;
-          let count = 0;
-          for (let i=6; i<temp.length-2; i+=8){   //Start at index 6. Last 2 elements are for day 6(we dont need it)
-            for (let j=i;  j< i + 8; j++){    //Loop through next 8 elements starting at index i
-              sum += temp[j];
-              count++;
-            }
-            const avg = sum/count;        //Calculate the average by dividing by 8
-            averages.push(avg);     //append
-            sum = 0;               //Reset sum and count for next iteration
-            count = 0;
-          }
+          const week = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+          const forecast = [];
+          let todaysDate = new Date().getDay()
 
-          console.log(averages)
-      
+          for (let i=0;i<5;i++){
+            let tempSum = 0
+            let count = 0
+            for (let dataPoint of data.list){
+              const date = new Date(dataPoint.dt*1000)
+              if (date.getDay() == todaysDate){
+                count++;
+                tempSum += dataPoint.main.temp;
+              }
+            }
+            const day={"dayName": week[todaysDate], "temp": Math.round(tempSum / count) }
+            forecast.push(day)
+            todaysDate = (todaysDate + 1) % 7
+          }
+          res.send({forecast})
+
+          
       });
   });
 
