9243f40c73939e8e8a874898406954b3e327c3f7
Yao(Victor) Li <victorliyao@gmail.com>
2023-05-15 18:36:15 -0700

    add cors

--- Practice_calling_API/script.js
+++ Practice_calling_API/script.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const request = require("request");
-
+const cors = require('cors');
 const app = express();
+app.use(cors());
+
+
 
 const API_KEY = "44410596c1e0b580b79b3679c28c51f8";
 
